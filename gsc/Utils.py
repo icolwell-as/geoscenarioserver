cd658d3a1d35a155b28991481b7d35f35ac7463b
Ian Colwell <ian.colwell@hexagon.com>
2024-07-12 14:05:00 -0400

    Consider 0 a positive number for scenario validation

--- gsc/Utils.py
+++ gsc/Utils.py
@@ -7,8 +7,8 @@ def get_number_range(str):
     range = str.split(':')
     if len(range) == 2:
         try:
-            range[0] = float(range[0]) 
-            range[1] = float(range[1]) 
+            range[0] = float(range[0])
+            range[1] = float(range[1])
             return range;
         except ValueError:
             return None
@@ -27,7 +27,7 @@ def is_valid_number_range(va,vb,vmin,vmax):
         return False
     return True;
 
-def is_valid_numberlist(vlist,min,max):    
+def is_valid_numberlist(vlist,min,max):
     for v in vlist:
         if not is_number(v):
             return False
@@ -35,7 +35,7 @@ def is_valid_numberlist(vlist,min,max):
             return False
 
 def is_positive_number(v):
-    return v is not None and is_number(v) and (float(v) > 0)
+    return v is not None and is_number(v) and (float(v) >= 0)
 
 def is_number(v):
     try:
